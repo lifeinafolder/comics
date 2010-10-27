03386b6e108d434f6c46248d5d14c65106032ae7
Rajat <ramittal@ebay.com>
2010-10-26 17:25:26 -0700

    Adding useful images. Adding Infinite Scroll. Using event delegation as infinite scroll can potentially cause infinite elements on page

--- js/ebayMasonry.js
+++ js/ebayMasonry.js
@@ -12,17 +12,45 @@ function loadImages(){
     appId: 'eBayb653a-8240-4cf1-9b3c-715b5b09f55',
     callback: function(response){
       var items = response.findItemsByKeywordsResponse["0"].searchResult["0"].item;
+      var len = items.length;
+      console.info('Items Fetched: ', len);
       var container = $('#box');
+      container.mouseover(function(e){
+        var target = $(e.target);
+        //console.log('Mouseover: ', target.attr('class'));
+        if ( target.hasClass('box') && !target.hasClass('highlight') ){
+          var infodialog = $(target).find('.info');
+          target.addClass('highlight');
+          infodialog.show();
+        }
+      });
+      
+      container.mouseout(function(e){
+        var mouseoutFrom = $(e.target);
+        var mouseoutTo = $(e.relatedTarget);
+        console.log('Mouseout from: ', mouseoutFrom.attr('class') );
+        console.log('Mouseout to: ', mouseoutTo.attr('class') );
+        
+        if( mouseoutFrom.hasClass('info') && mouseoutTo !== mouseoutFrom.parent() ){
+          var highlightedElem = $(this).find('.highlight');
+          highlightedElem.removeClass('highlight');
+          mouseoutFrom.hide();
+        }
+      });
+      
+      var fragment = document.createDocumentFragment();
       items.forEach(function(val,ind){
         if ( val.galleryURL ){
+          //var source = val.galleryPlusPictureURL['0'] || val.galleryURL["0"];
           var source = val.galleryURL["0"];
           source = source.replace('8080_1','4040_1');
+          var link = val.viewItemURL[0];
           var img = new Image();
           img.onload = function(){
             var wrapper = document.createElement('DIV');
             $(wrapper).css({
               'width': img.width,
-              'height': img.height,
+              'height': img.height
             });
             var info = document.createElement('DIV');
             $(info).css({
@@ -34,63 +62,80 @@ function loadImages(){
             $(wrapper).append(info);
             $(wrapper).addClass('box');
             wrapper.style.backgroundImage = 'url(' + source + ')';
-            
+
+            /*
             $(wrapper).mouseenter( function(){
               var self = this;
               $(self).addClass('highlight');
-              $(this).find('.info').show();
+              $(info).show();
             });
-      
+
             $(wrapper).mouseleave( function(){
               $(this).removeClass('highlight');
-              $(this).find('.info').hide();
+              $(info).hide();
             });
-            
-            container.append(wrapper);
+*/
+            $(wrapper).click(function(){
+              window.location.href = link;
+            });
+
+            wrapper.style.opacity = 0;
+            fragment.appendChild(wrapper);
+            $(wrapper).animate({
+              opacity:1
+            },1000);
+
+            if(fetching){
+              fetching = false;
+              loadingDiv.hide();
+            }
           };
           img.src = source;
         }
       });
-
-
-      var target;
-      $(document).keyup(function(e){
-        if ( !target ){
-          target = $(container).find(':first');
-          $(target).addClass('highlight');
+      
+      //single injection in the DOM causing less reflow
+      var itemsInjected = 0;
+      var timer = setInterval(function(){
+        if( itemsInjected === len ){
+          clearInterval(timer);
           return;
         }
-        $(target).removeClass('highlight');        
-        switch( e.keyCode ){
-          case 37:  //left
-            target = ( $(target).prev().length !== 0 ) ? $(target).prev() : target;
-            break;
-          case 38:
-            var offset = $(target).offset();
-            var top = offset.top - ($(target).outerHeight() + 20);
-            var newTarget = document.elementFromPoint(offset.left,top);
-            target = newTarget;
-          case 39:  //right
-            target = ( $(target).next().length !== 0 ) ? $(target).next() : target;
-            break;
-          case 40:
-            var offset = $(target).offset();
-            var top = offset.top + $(target).outerHeight() + 20;
-            var newTarget = document.elementFromPoint(offset.left,top);
-            target = newTarget;
-            break;
+        else{
+          console.info('Items Injected: ', fragment.childNodes.length);
+          itemsInjected += fragment.childNodes.length;
+          container.append(fragment);
         }
-        $(target).addClass('highlight');
-      });
+      },500);
+      //setTimeout(doMasonry,100);
     }
   });
 
-  e.searchByKeywords('gucci dress');
+  var pageNo = 1, fetching = false;
+  $(document).scroll(function(){
+    var pageHeight = $(document).height();
+    var viewportHeight = $(window).height();
+    var scrollHeight = $(document).scrollTop();
+
+    if ( !fetching && (pageHeight - viewportHeight - scrollHeight < 100) ){
+      e.searchByKeywords('comic books batman',{
+        'paginationInput.pageNumber' : (pageNo+1)
+      });
+      fetching = true;
+      loadingDiv.show();
+    }
+  });
+
+  e.searchByKeywords('comic books batman');
+  fetching = true;
 }
 
-function doMasonry(){
+var loadingDiv = $('footer');
+
+function doMasonry(newcontent){
   $('#box').masonry({
-    columnWidth: 10
+    columnWidth: 10,
+    appendedContent: newcontent
   });
 }
 
